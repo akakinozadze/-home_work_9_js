5cc37b92e3604551e78766078848adbca6c49af2
akakinozadze <145987347+akakinozadze@users.noreply.github.com>
2024-02-26 16:43:50 +0400

    done

--- js/script.js
+++ js/script.js
@@ -4,25 +4,34 @@ const mainBox = document.getElementById("mineBox");
 const Overlay = document.getElementById("overlay");
 const ContentPh = document.getElementById("phConteiner");
 const close = document.getElementById("close");
+const post = document.getElementById("post");
+const OverlayADD = document.getElementById("postAdd");
+const form = document.getElementById("form-overlay");
+const close2 = document.getElementById("close2");
 
-function xmlFunction(linki, gaparsuliInfoCallback) {
-  const xml = new XMLHttpRequest();
-  xml.open("GET", linki);
-  xml.addEventListener("load", function () {
-    const gaparsuliInfo = JSON.parse(this.responseText);
-
-    gaparsuliInfoCallback(gaparsuliInfo);
-  });
-  xml.send();
-}
-xmlFunction(
-  "https://jsonplaceholder.typicode.com/posts",
-  function (rendomSaxeli) {
-    rendomSaxeli.forEach((element) => {
-      titleInfo(element);
+function fetch1(url, callback) {
+  fetch(url, {
+    method: "GET",
+  })
+    .then(function (item) {
+      if (item.status !== 200) {
+        throw item.status;
+      }
+      return item.json();
+    })
+    .then(function (respons) {
+      callback(respons);
+    })
+    .then(function (error) {
+      if (error == 404) {
+        console.log("Page Not Found");
+      } else if (error == 500) {
+        console.log("Server Error");
+      }
     });
-  }
-);
+}
+
+fetch1("https://jsonplaceholder.typicode.com/posts", forEachFnc);
 
 function titleInfo(item) {
   const divConteiner = document.createElement("div");
@@ -36,15 +45,13 @@ function titleInfo(item) {
 
   // წაშლის ღილაკი
   const button = document.createElement("button");
-  button.innerText = "Derete Post";
+  button.innerText = "Delete Post";
   button.setAttribute("item-id", item.id);
   button.addEventListener("click", function (e) {
     e.stopPropagation();
     const delete1 = e.target.getAttribute("item-id");
-    // console.log(delete1);
     deleteProperti(delete1);
   });
-
   divConteiner.appendChild(h3);
   divConteiner.appendChild(h2);
   divConteiner.appendChild(button);
@@ -54,15 +61,12 @@ function titleInfo(item) {
     // console.log(postId);
     Overlay.classList.add("activeOverlay");
     const newUrlPost = `https://jsonplaceholder.typicode.com/posts/${postId}`;
-    xmlFunction(newUrlPost, function (element) {
+    fetch1(newUrlPost, function (element) {
       detInfoWamogeba(element);
     });
   });
-
   mainBox.appendChild(divConteiner);
-
   //პოსტის წაშლა ფუნქცია
-
   function deleteProperti(delete1) {
     const deleteUrl = `https://jsonplaceholder.typicode.com/posts/${delete1}`;
     fetch(deleteUrl, {
@@ -73,8 +77,16 @@ function titleInfo(item) {
     });
   }
 }
-//  პოსტის ინფორმაციის დეტალური წამოღება
 
+// ასი პოსტის ფუნქცია
+function forEachFnc (rendomSaxeli) {
+  rendomSaxeli.forEach((element) => {
+    titleInfo(element);
+  });
+}
+
+
+//  პოსტის ინფორმაციის დეტალური წამოღება
 function detInfoWamogeba(item) {
   const pDescrp = document.createElement("p");
   pDescrp.classList.add("pharagrap1");
@@ -85,4 +97,35 @@ function detInfoWamogeba(item) {
 close.addEventListener("click", function () {
   Overlay.classList.remove("activeOverlay");
   ContentPh.innerHTML = " ";
+});
+
+// პოსტის დამატება
+post.addEventListener("click", function () {
+  OverlayADD.classList.add("overlayInside");
+});
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault(); // დასაბმითების (პოსტის გაგზავნის დროს) დროს რომ არ დარესტარტდეს გვერდი
+  let formSendInfo = {
+    title: e.target.value,
+    userid: 11,
+  };
+  fetch("https://jsonplaceholder.typicode.com/posts", {
+    method: "POST",
+    body: JSON.stringify(formSendInfo),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+    .then((response) => response.json())
+    .then((sendObj) => {
+      // გაგზავნას რომ დააწვება დაიხურება გვერდი
+      titleInfo(sendObj); // დომში დაამატებს 101 ობიექტს (გაგზავნილი პოსტი დაემატება)
+      OverlayADD.classList.remove("overlayInside");
+      e.target[0].value = ""; //  გაასუფთავებს ველიუს
+    });
+});
+// პოსტის გაგზავნის გვერდის დახურვა
+close2.addEventListener("click", function () {
+  OverlayADD.classList.remove("overlayInside");
 });
